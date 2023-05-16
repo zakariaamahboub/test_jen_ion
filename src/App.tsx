9c8ae1dd5acfe2dac4baaa166cc14f4c3d84dcdd
zakariaa <zakariaa.formisa@gmail.com>
2023-05-16 12:06:34 +0200

    te

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ const App: React.FC = () => (
           <Route path="/tab3" component={Tab3} />
           <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
         </IonRouterOutlet>
+        <
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
